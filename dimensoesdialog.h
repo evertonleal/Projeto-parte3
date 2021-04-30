c871cda0851657091fc9cac421258d28d1662c3a
evertonleal <39661186+evertonleal@users.noreply.github.com>
2021-04-30 02:32:04 -0300

    Update dimensoesdialog.h

--- dimensoesdialog.h
+++ dimensoesdialog.h
@@ -1,29 +1,38 @@
 #ifndef DIMENSOESDIALOG_H
 #define DIMENSOESDIALOG_H
 
-#include <QDialog>
-#include <QSpinBox>
+#include "sculptor.h"
+#include <QWidget>
 
-namespace Ui {
-class DimensoesDialog;
-}
-
-class DimensoesDialog : public QDialog
+class DimensoesDialog : public QWidget
 {
     Q_OBJECT
+public:explicit DimensoesDialog(QWidget *parent = nullptr);
+    void paintEvent(QPaintEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void on_pushButton_clicked();
+    void putVoxel();
+    void cutVoxel();
+    void putBox(int x1, int y1, int z1);
+    void cutBox(int x1, int y1, int z1);
+    void putSphere(int r);
+    void cutSphere(int r);
+    void putEllipsoid(int rx, int ry, int rz);
+    void cutEllipsoid(int rx, int ry, int rz);
+    void setRGBA(int r, int g, int b, int a);
+    void setZAtual(int z);
+    void setRadioButton(int x);
+signals:
+    void posAtualMouse(int x,int y);
 
-public:
-    explicit DimensoesDialog(QWidget *parent = nullptr);
-    ~DimensoesDialog();
-    int leDimX();
-    int leDimY();
-    int leDimZ();
-
-private slots:
+public slots:
+    void setSizeMatriz(int x, int y, int z);
 
 private:
-    Ui::DimensoesDialog *ui;
-
+    bool matrizCriada;
+    Sculptor *matriz;
+    int matrizX, matrizY, matrizZ, XAtual,YAtual,ZAtual,XRelease,YRelease,sizeRectX,sizeRectY,selectedButton;
 };
 
 #endif // DIMENSOESDIALOG_H
