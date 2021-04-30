34a0c662cae1ad71ac0281da64bac7492bb8ac39
evertonleal <39661186+evertonleal@users.noreply.github.com>
2021-04-30 02:01:10 -0300

    Create desenho.h

---
+++ desenho.h
@@ -0,0 +1,28 @@
+#ifndef DESENHO_H
+#define DESENHO_H
+#include "Sculptor.h"
+#include <QWidget>
+
+class Desenho : public QWidget
+{
+    Q_OBJECT
+private:
+    int nx=10,ny=10,nz=10;
+
+public:
+    explicit Desenho(QWidget *parent = nullptr);
+    void paintEvent(QPaintEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    void setX(int);
+    void setY(int);
+    void setZ(int);
+    Sculptor *dims;
+
+signals:
+    void posicaoMouse(int, int);
+
+public slots:
+
+};
+
+#endif // DESENHO_H
